b5b5a71618c376ebd01b64931ebe158865f926c9
Caio Lopes <caiotuxo@gmail.com>
2021-01-20 16:56:12 -0300

    gem uopdate

--- lib/m2y_sinqia/modules/sinqia_documents.rb
+++ lib/m2y_sinqia/modules/sinqia_documents.rb
@@ -6,20 +6,42 @@ module M2ySinqia
       startModule(access_key, secret_key, env)
     end
 
+    def getDocumentHeaders
+      url = address_url + USER_ADDRESS_PATH
+      puts url
+      response = HTTParty.get(url, basic_auth: @request.auth)
+
+      headers = {}
+
+      token = response.headers["set-cookie"]
+      if !token.nil?
+        token = token.split("XSRF-TOKEN=").last.split(";").first
+        puts token
+        headers["X-XSRF-TOKEN"] = token
+        headers["Cookie"] = "XSRF-TOKEN=#{token}"
+      end
+      puts headers
+      headers
+    end
+
+
     def addKit(body)
-      response = @request.post(documents_url + ADD_KIT, body)
+      headers = getDocumentHeaders
+      response = @request.post(documents_url + ADD_KIT, body, headers)
       puts response
       SinqiaModel.new(response)
     end
 
     def checkKit(params)
-      response = @request.post(documents_url + CHECK_KIT, params)
+      headers = getDocumentHeaders
+      response = @request.post(documents_url + CHECK_KIT, params, headers)
       puts response
       SinqiaModel.new(response)
     end
 
     def sendProposal(params)
-      response = @request.post(documents_url + SEND_PROPOSAL, params)
+      headers = getDocumentHeaders
+      response = @request.post(documents_url + SEND_PROPOSAL, params, headers)
       puts response
       SinqiaModel.new(response)
     end
@@ -27,5 +49,10 @@ module M2ySinqia
     def documents_url
       @url.gsub('8090', '8091')
     end
+
+    def address_url
+      @url.gsub('8090', '8093')
+    end
+
   end
 end
